8282314a4c28bce0bff71e691013a9209b2ead72
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-05-03 19:16:15 -0300

    chore: create books table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,16 @@ CREATE TABLE publishers (
     publishers_id SERIAL PRIMARY KEY,
     publishers_name VARCHAR(100) NOT NULL
 );
+
+-- Books Table
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    release_year INT,
+    pages INT,
+    isbn VARCHAR(20),
+    author_id INT REFERENCES authors(author_id),
+    topic_id INT REFERENCES topics(topic_id),
+    languages_id INT REFERENCES languages(languages_id),
+    publishers_id INT REFERENCES publishers(publishers_id)
+);
