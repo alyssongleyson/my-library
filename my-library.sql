4ea591334d1d87b9a95952fc120753252949e797
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-05-02 23:05:10 -0300

    chore: create topics table

--- my-library.sql
+++ my-library.sql
@@ -9,3 +9,9 @@ CREATE TABLE authors {
     author_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 };
+
+-- Topics Table
+CREATE TABLE topics (
+    topic_id SERIAL PRIMARY KEY,
+    topic_name VARCHAR(100) NOT NULL
+);
