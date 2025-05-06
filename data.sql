d8e5d86cae4f222a0812db783f1502a83fe61001
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-05-06 19:33:10 -0300

    chore: insert book data into books table

--- data.sql
+++ data.sql
@@ -18,3 +18,7 @@ INSERT INTO topics (topic_name) VALUES
 ('CSS'),
 ('JavaScript'),
 ('Python');
+
+-- Insert Books
+INSERT INTO books (title, release_year, pages isbn, author_id, topic_id, languages_id, publishers_id) VALUES
+('Como o Linux Funviona: O que todo superusuário deveria saber', 2017, 440, '978-85-7522-419-9', 1, 1, 1, 1);
